9da062862027f988b45234037a0d38e9f51641f4
Henry S. Sjøen <hsjo@equinor.com>
2020-02-27 07:22:35 +0100

    Change the port to 3000

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
     app.enableCors();
-    await app.listen(process.env.PORT || 3001);
+    await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
